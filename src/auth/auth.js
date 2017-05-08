d886830e3a162854d01c3cf108443995a4c430ec
Sebastian Petak <sebastianpetak@gmail.com>
2017-05-08 10:50:46 -0500

    Finish auth.js

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -15,14 +15,57 @@ export default {
 	login(context, creds, redirect) {
 		context.$http.post(LOGIN_URL, creds, (data) => {
 			localStorage.setItem('id_token', data.id_token);
-			localStorage.setItem('access_token', data.access_token)
+			localStorage.setItem('access_token', data.access_token);
 
-			this.user.authenticated = transform-runtime
+			this.user.authenticated = true;
 
 			// Redirect to a specified route
 			if(redirect) {
-				router.go(redirect)
+				router.go(redirect);
 			}
+
+		}).error((err) => {
+			context.error = err;
 		});
+	},
+
+	signup(context, creds, redirect) {
+		context.$http.post(SIGNUP_URL, creds, (data) => {
+			localStorage.setItem('id_token', data.id_token);
+			localStorage.setItem('access_token', data.access_token);
+
+			this.user.authenticated = true;
+
+			if(redirect) {
+				router.go(redirect);
+			}
+
+		}).error((err) => {
+			context.error = err;
+		});
+	},
+
+	// to log out, we just need to remove the token
+	logout() {
+		localStorage.removeItem('id_token');
+		localStorage.removeItem('access_token');
+		this.user.authenticated = false;
+	},
+
+	checkAuth() {
+		var jwt = localStorage.getItem('id_token');
+		if(jwt) {
+			this.user.authenticated = true;
+		} else {
+			this.user.authenticated = false;
+		}
+	},
+
+	// the object to be passed as a header for authenticated requests
+	getAuthHeader() {
+		return {
+			'Authorization': 'Bearer ' + localStorage.getItem('access_token')
+		};
 	}
+
 };
