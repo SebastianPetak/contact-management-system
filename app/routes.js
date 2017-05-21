687c86cb024e5470604ca4bf0d69a9d02c195499
Sebastian Petak <sebastianpetak@gmail.com>
2017-05-21 16:27:15 -0500

    Add contact page Add contact page for users and api.  Add routes for contact page.

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,35 @@
+var Contact = require('./models/Contact.model');
+
 module.exports = function(app) {
 	// INDEX PAGE ============================================================
 	app.get('/', function(req, res) {
 		res.render('index.ejs');
+	});
+
+	// CONTACTS PAGE =========================================================
+	// get all contacts in json
+	app.get('/contacts-api', function(req,res) {
+		Contact.find({})
+		.exec()
+		.then(function(contacts) {
+			res.json(contacts);
+		})
+		.catch(function(err) {
+			res.status(500).send({ error: 'Error occured' });
+		});
+	});
+
+	// get the html contact page
+	app.get('/contacts', function(req,res) {
+		Contact.find({})
+		.exec()
+		.then(function(contacts) {
+			res.render('contacts', {
+				contacts: contacts
+			});
+		})
+		.catch(function(err) {
+			res.status(500).send({ error: 'Error occured' });
+		});
 	});
 };
